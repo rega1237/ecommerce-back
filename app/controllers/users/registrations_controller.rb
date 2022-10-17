591c09eedd5b71342b9dff7e4d33a4c4f2a3e285
Rafael Guzman <rega1237@gmail.com>
2022-10-17 17:41:58 -0400

    add methods for registration and sessions controllers

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, options={})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: 'Signed up successfully', data: resource }
+      }, status: :ok
+    else
+      render json: {
+        status: { message: 'User could not be created successfull', errors: resource.errors.full_messages }, status: :unprocessable_entity
+      }
+    end
+  end 
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
