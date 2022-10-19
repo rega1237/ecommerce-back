b3e1cea1bd3d27ecca1cdb981017b4762d53c5dc
Rafael Guzman <rega1237@gmail.com>
2022-10-19 17:11:26 -0400

    create buy_sell model

---
+++ db/migrate/20221019210504_create_buy_sells.rb
@@ -0,0 +1,11 @@
+class CreateBuySells < ActiveRecord::Migration[7.0]
+  def change
+    create_table :buy_sells do |t|
+      t.date :date
+      t.references :user, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
