ea47384f20de37cbeaed67b335d7115100c5c39d
Rafael Guzman <rega1237@gmail.com>
2022-10-19 17:11:50 -0400

    write relationship between models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :comments
   has_many :users, through: :comments, dependent: :destroy
+  has_many :buy_sells
+  has_many :users, through: :buy_sells, dependent: :destroy
 end
