a2d5658672e4a2cd14ea58711b38e863d1c20959
Rafael Guzman <rega1237@gmail.com>
2022-10-23 18:00:27 -0400

    add method to update bought times

--- app/models/buy_sell.rb
+++ app/models/buy_sell.rb
@@ -1,4 +1,13 @@
 class BuySell < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  after_save :update_product
+
+  private 
+  
+  def update_product
+    product = Product.find(product_id)
+    product.update(bought_times: product.bought_times + 1)
+  end
 end
